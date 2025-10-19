557159270f7511051fff914ce52b28844b16a4ec
thanlinardos <thanlinardos@yahoo.gr>
2025-10-19 22:04:43 +0300

    fix discovery server test

--- resource-server/src/test/java/com/thanlinardos/resource_server/ResourceServerApplicationTests.java
+++ resource-server/src/test/java/com/thanlinardos/resource_server/ResourceServerApplicationTests.java
@@ -2,11 +2,8 @@ package com.thanlinardos.resource_server;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
-@TestPropertySource("classpath:application-test.properties")
+@SpringTest
 class ResourceServerApplicationTests {
 
     @Test
